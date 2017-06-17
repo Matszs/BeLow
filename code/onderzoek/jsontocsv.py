eda0e95aad142cd0faf3f0b4e0c1a5f2d1cce1b8
Patrick Hendriks <p.hendriks1990@gmail.com>
2017-06-17 12:21:08 +0200

    Changed script to parse date and tranform to epoch time for better handling in gnuplot. also changed format for gnuplot instead of csv

--- code/onderzoek/jsontocsv.py
+++ code/onderzoek/jsontocsv.py
@@ -1,15 +1,17 @@
 import json
+from dateutil import parser
 
-with open('data.json') as data_file:
+
+with open('school_temp_door_22-5-17_26-5-17.json') as data_file:
     data = json.load(data_file)
 
-f = open("data.csv", "w")
+f = open("gnuplot_fmt.dat", "w")
 
 for d in data['data']:
-    time = data['data'][d]['DevEUI_uplink']['Time']
+    time = dt = parser.parse(data['data'][d]['DevEUI_uplink']['Time'])
     payload = bytearray.fromhex(data['data'][d]['DevEUI_uplink']['payload_hex']).decode().split('|')
     temp = float(payload[1]) / 100
 
     if temp < 100.0:
-        print("{:s},{:s},{:.2f}".format(time, payload[0],temp))
-        f.write("{:s},{:s},{:.2f}\n".format(time, payload[0],temp))
+        print("{:d} {:s} {:.2f}".format(int(time.timestamp()), payload[0],temp))
+        f.write("{:d} {:s} {:.2f}\n".format(int(time.timestamp()), payload[0],temp))
